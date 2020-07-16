0ebc5bba71676d61fc506da51faf3e543e0bec4e
jeffjramos21 <49539423+jeffjramos21@users.noreply.github.com>
2020-07-16 16:13:25 -0400

    Add files via upload

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -6,62 +6,22 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 export function AboutScreen(navigation){
   return (
     <View style={styles.container}>
-      <Text style={styles.descriptionText}>Meridian is a money management application that
-                                 allows users to create, read from, update, and
-                                 delete groups that contain other users that have
-                                 a financial relationship to the original user.
-                                 Meridian allows users to keep track of
-                                 finances for all kinds of events. From vacations
-                                 to dinners, from loans to everyday expenses, this
-                                 application provides a platform to conveniently
-                                 manage your finances.
-                                 </Text>
-
-                                 <Text style={styles.devText}>
-                                 Developers:
-                                 </Text>
-
-                                 <Text style={styles.creditsText}>
-                                 Nas Lyazghi
-                                 </Text>
-                                 <Text style={styles.creditsText}>
-                                 Jeffrey Ramos
-                                 </Text>
-                                 <Text style={styles.creditsText}>
-                                 Ryan Gordon
-                                 </Text>
-                                 <Text style={styles.creditsText}>
-                                 Erik Bates
-                                 </Text>
-                                 <Text style={styles.creditsText}>
-                                 Austin Traub
-                                 </Text>
-                                 <Text style={styles.creditsText}>
-                                 Moises "David" Cortes Lugo
-                                 </Text>
+      <Text style={styles.text}>About</Text>
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 30
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
-  descriptionText: {
-    fontSize: 20,
+  text: {
+    fontSize: 32,
     color: 'white',
   },
-
-  devText: {
-    paddingTop: 300,
-    fontSize: 14,
-    color: 'white'
-  },
-
-  creditsText: {
-    fontSize: 14,
-    color: 'white'
-  }
 });
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,14 +1,64 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {Alert, View, StyleSheet, Text} from 'react-native';
+import LoginScreen from '../screens/LoginScreen'
+import {AsyncStorage} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Avatar } from "react-native-elements";
+const BASE_URL = 'https://cop4331-test-2.herokuapp.com/';
 
-export function ProfileScreen(navigation){
+let username;
+let email;
+
+var getUsername = async () => {
+  try {
+    username = await AsyncStorage.getItem('@username_Key')
+    if (username === null) {
+      Alert.alert('error on username retrieval')
+    }
+  } catch (e) {
+    Alert.alert('failure')
+  }
+}
+
+var getEmail = async () => {
+  try {
+    email = await AsyncStorage.getItem('@email_Key')
+    if (email === null) {
+      Alert.alert('error on email retrieval')
+    }
+  } catch (e) {
+    Alert.alert('failure')
+  }
+}
+
+var call = (num) => {
+  return 1;
+}
+
+var usernameSync = (func) => {
+  let num = 5
+  function callBack(num)
+  {
+    func(num);
+  }
+
+  getUsername();
+}
+
+usernameSync(call(1))
+
+export function ProfileScreen({navigation, props, token}){
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Profile Screen</Text>
+    <Avatar size = "xlarge"
+      overlayContainerStyle={{backgroundColor: '#009387'}}
+      rounded title={username.substring(0,1)}/>
+      <Text style={styles.nameText}> {username}</Text>
+      <Text style={styles.email}> {email}</Text>
     </View>
   );
+
 }
 
 
@@ -17,11 +67,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60
   },
 
-  text: {
-    fontSize: 32,
-    color: 'white',
+  nameText: {
+    fontSize: 48,
+    paddingTop: 60,
+    color: 'white'
   },
+
+  email: {
+    fontSize: 24,
+    paddingTop: 20,
+    color: 'white'
+  }
 });
