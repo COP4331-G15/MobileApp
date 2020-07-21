ed3e975a9d119e0ff1bb9ed199d2805018914149
naslyazghi <54712901+naslyazghi@users.noreply.github.com>
2020-07-21 09:31:48 -0400

    Update LoginScreen.js

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,94 +1,314 @@
 import React from 'react';
-import {View, StyleSheet, Text, Alert} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import GroupList from '../lists/GroupsList';
+import {
+  View,
+  StyleSheet,
+  Text,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import {Heading} from '../components/Heading';
+import {FilledButton} from '../components/FilledButton';
+import {TextButton} from '../components/TextButton';
+import {Error} from '../components/Error';
+import {AuthContainer} from '../components/AuthContainer';
+import {AuthContext} from '../contexts/AuthContext';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import * as Animatable from 'react-native-animatable';
 import jwt_decode from 'jwt-decode';
-import cloneDeep from 'lodash/cloneDeep';
-const BASE_URL = 'https://cop4331-test-2.herokuapp.com/draftapi/user/';
+const BASE_URL = 'https://cop4331-test-2.herokuapp.com/';
 
-global.userId = null;
-global.groups = null;
-global.groupsLength = null;
+const LoginScreen = ({navigation}) => {
+  // Declare variables ans states
+  const [data, setData] = React.useState({
+    email: '',
+    password: '',
+    check_textInputChange: false,
+    secureTextEntry: true,
+    isValidUser: true,
+    isValidPassword: true,
+    message: '',
+  });
 
-export function MainScreen({route, navigation}) {
-  // Set the groups variable
-  const [groups, setGroups] = React.useState(null);
+  const {logIn} = React.useContext(AuthContext);
 
-  // Get the token;
-  const {token} = route.params;
-
-  // Decode the token
-  var jwt_decode = require('jwt-decode');
-  var decoded = jwt_decode(token);
-
-  // Create the user object
-  var user = {
-    id: decoded.id,
-    username: decoded.name,
-    email: decoded.email,
-  };
-
-  global.userId = user.id;
-  global.token = token;
-  
-
-  // Retrieve groups
-  const handleGroup = async () => {
-    try {
-      // Respone variable from the API
-      const response = await fetch(BASE_URL + user.id + '/groups', {
-        method: 'GET',
-        headers: {'Content-Type': 'application/json', Authorization: token},
+  // email has to be atleast 4 charachters long
+  const textInputChange = val => {
+    if (val.trim().length >= 8) {
+      setData({
+        ...data,
+        email: val,
+        check_textInputChange: true,
+        isValidUser: true,
       });
-
-      // Parse the response
-      var txt = await response.text();
-      var res = JSON.parse(txt);
-
-      // Process the response
-      // No permission
-      if (response.status === 401) {
-        Alert.alert('Permission Error', response.status, [{text: 'OK'}]);
-      }
-      // Error
-      else if (response.status !== 200) {
-        Alert.alert('Error', response.status, [{text: 'OK'}]);
-      }
-      // Success, Data found!!
-      else {
-        //setGroups(cloneDeep(res.groups));
-        setGroups(res.groups);
-        global.groups = res.groups;
-        global.groupsLength = res.groups.length;
-        // console.log('Main screen, global.groups = ');
-        // console.log(global.groups.length);
-
-      }
-    } catch (e) {
-      Alert.alert('Error', e.toString(), [{text: 'OK'}]);
+    } else {
+      setData({
+        ...data,
+        email: val,
+        check_textInputChange: false,
+        isValidUser: false,
+      });
     }
   };
 
-  handleGroup();
+  // Password has to be at least 8 charachters long
+  const handlePasswordChange = val => {
+    if (val.trim().length >= 6) {
+      setData({
+        ...data,
+        password: val,
+        isValidPassword: true,
+      });
+    } else {
+      setData({
+        ...data,
+        password: val,
+        isValidPassword: false,
+      });
+    }
+  };
 
-  //console.log('--- data = ' + groups);
+  // Switch the secure Text Entry for the password
+  const updateSecureTextEntry = () => {
+    setData({
+      ...data,
+      secureTextEntry: !data.secureTextEntry,
+    });
+  };
+
+  const handleValidUser = val => {
+    if (val.trim().length >= 8) {
+      setData({
+        ...data,
+        isValidUser: true,
+      });
+    } else {
+      setData({
+        ...data,
+        isValidUser: false,
+      });
+    }
+  };
+
+  // logIn
+  const loginHandle = async (email, password) => {
+    
+    // Construct the Json body for the request
+    const js = '{"email":"' + email + '","password":"' + password + '"}';
+
+    // email or Password is empty
+    if (email.length == 0 || password.length == 0) {
+      Alert.alert('Wrong Input!', 'email or password field cannot be empty.', [
+        {text: 'OK'},
+      ]);
+      return;
+    }
+
+    try {
+      // 1 - Respone variable from the API
+      const response = await fetch(BASE_URL + 'api/users/login', {
+        method: 'POST',
+        body: js,
+        headers: {'Content-Type': 'application/json'},
+      });
+
+      // 2 - Parsing the response
+      var res = JSON.parse(await response.text());
+
+      // 3 - Processing the response
+      // User not found
+      if (res.id <= 0) {
+        //setMessage('User/Password combination incorrect');
+        setData({
+          ...data,
+          message: 'User/Password combination incorrect',
+        });
+        // Show an alert box
+        Alert.alert('Error', 'User/Password combination incorrect', [
+          {text: 'OK'},
+        ]);
+        return;
+      }
+      // User found
+      else {
+        // Decode the token
+        var jwt_decode = require('jwt-decode');
+        var decoded = jwt_decode(res.token);
+        //localStorage.setItem('user_data', JSON.stringify(user));
+
+        // Set the message
+        setData({
+          ...data,
+          message: 'Login is successful',
+        });
+
+        // Store the user Info
+        var user = {
+          id: decoded.id,
+          username: decoded.name,
+          email: res.email,
+          userToken: res.token,
+        };
+        
+
+        // Show an alert box
+        Alert.alert(
+          'Welcome to Meridian',
+          'You have been successfully logged in' + 
+            '\nUsername: ' +
+            decoded.name +
+            '\nEmail: ' +
+            res.email,
+          +[{text: 'OK'}],
+        );
+
+        logIn(user);
+
+        return;
+        // Direct the user to the main screen
+        // window.location.href = '/cards';
+      }
+    } catch (e) {
+      Alert.alert('Error', e.toString(), [{text: 'OK'}]);
+      return;
+    }
+
+    // if ( foundUser.length == 0 ) {
+    //   Alert.alert('Invalid User!', 'email or password is incorrect.', [
+    //     {text: 'Okay'}
+    // ]);
+    // return;
+    // }
+
+    // logIn(email, password);
+  };
 
   return (
     <View style={styles.container}>
-      <GroupList itemList={groups} />
+      {/*Show Status bar on top of the screen*/}
+      <StatusBar backgroundColor="#1B1921" barstyle="light-content" />
+      {/*Title of the page*/}
+      <Heading style={styles.title}>Login</Heading>
+
+      {/*Error Message*/}
+      <Error error={''} />
+
+      {/*email*/}
+      <View style={styles.action}>
+        <FontAwesome name={'user-o'} color="white" size={22} />
+        <TextInput
+          style={styles.inputemail}
+          placeholder={'email'}
+          placeholderTextColor={'grey'}
+          onChangeText={val => textInputChange(val)}
+          onEndEditing={e => handleValidUser(e.nativeEvent.text)}
+        />
+        {data.check_textInputChange ? (
+          <Animatable.View animation="bounceIn">
+            <Feather name="check-circle" color="#009387" size={20} />
+          </Animatable.View>
+        ) : null}
+      </View>
+      {/*Show error messgae for a non valid email*/}
+      {data.isValidUser ? null : (
+        <Animatable.View animation="fadeInLeft" duration={500}>
+          <Text style={styles.errorMsg}>email must be 8 characters long.</Text>
+        </Animatable.View>
+      )}
+
+      {/*Password*/}
+      <View style={styles.action}>
+        <Feather
+          style={styles.fontAwesome}
+          name={'lock'}
+          color="white"
+          size={22}
+        />
+        <TextInput
+          style={styles.inputemail}
+          placeholder={'Password'}
+          placeholderTextColor={'grey'}
+          secureTextEntry={data.secureTextEntry ? true : false}
+          onChangeText={val => handlePasswordChange(val)}
+        />
+        <TouchableOpacity onPress={updateSecureTextEntry}>
+          {data.secureTextEntry ? (
+            <Feather name="eye-off" color="grey" size={20} />
+          ) : (
+            <Feather name="eye" color="white" size={20} />
+          )}
+        </TouchableOpacity>
+      </View>
+      {/*Show error messgae for a non valid email*/}
+      {data.isValidPassword ? null : (
+        <Animatable.View animation="fadeInLeft" duration={500}>
+          <Text style={styles.errorMsg}>
+            Password must be 6 characters long.
+          </Text>
+        </Animatable.View>
+      )}
+
+      {/*Forgot password*/}
+      <TextButton
+        style={[{alignItems: 'flex-end'}, {marginTop: 30}]}
+        title={'Forgot Password?'}
+        onPress={() => {}}
+      />
+
+      {/*Login Button*/}
+      <FilledButton
+        title={'Login'}
+        style={styles.loginButton}
+        onPress={() => {
+          loginHandle(data.email, data.password);
+        }}
+      />
+      {/*onPress={() => navigation.navigate()}/>*/}
+
+      {/*Create an account*/}
+      <TextButton
+        title={"Don't have an account?  Create one"}
+        onPress={() => navigation.navigate('RegisterScreen')}
+      />
     </View>
   );
-}
+};
+
+export default LoginScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 5,
+    alignItems: 'center',
+    paddingTop: 100,
+    backgroundColor: '#1B1921',
+    padding: 20,
   },
 
-  text: {
-    fontSize: 32,
+  title: {
+    marginBottom: 30,
+  },
+
+  loginButton: {
+    marginVertical: 23,
+  },
+  inputemail: {
+    fontSize: 16,
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -12,
+    paddingLeft: 10,
     color: 'white',
+  },
+  action: {
+    flexDirection: 'row',
+    marginTop: 35,
+    borderBottomWidth: 1,
+    borderBottomColor: '#009387',
+  },
+  errorMsg: {
+    color: '#FF871D',
+    fontSize: 14,
   },
 });
